84157e69f08e7c05a15908a73366bfc1e5654388
NeelQA <31629960+NeelQA@users.noreply.github.com>
2018-10-29 11:17:39 +0530

    updated the texts

--- tests/navigation.ts
+++ tests/navigation.ts
@@ -3,8 +3,8 @@ import { browser } from "protractor";
 const resetUrl = "data:text/html,<html></html>";
 
 /**
- * Navigates to the given URL.
- * Note: The current page is cleared in order to ensure the application doesn't intercept the navigation event.
+ * Navigates to the given URL. Note: The current page is cleared in order to ensure the application
+ * doesn't intercept the navigation event.
  * @param url Url to navigate to.
  */
 export async function navigateTo(url: string): Promise<void> {
